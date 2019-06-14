90453a7c54452cdb6fa631780e045b86982471bb
Matt Bond <matt+git@codefire.io>
2019-06-14 12:48:23 -0400

    Complete "Hello World"

---
+++ hello-world/hello_world.go
@@ -0,0 +1,9 @@
+/*
+Package greeting provides the traditional new programming language greeting.
+*/
+package greeting
+
+// HelloWorld returns the greeting as a string.
+func HelloWorld() string {
+	return "Hello, World!"
+}
