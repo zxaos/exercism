725b69f0e26b4ec319bee41c573e384a7ba65ec8
Matt Bond <matt+git@codefire.io>
2019-06-23 17:06:23 -0400

    adjust hamming to use runes consistently

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,20 +1,18 @@
 // package hamming provides utilities for working with utf8 representations of dna strings
 package hamming
 
-import (
-	"errors"
-	"unicode/utf8"
-)
+import "errors"
 
 // Distance calculates the Hamming distance between two DNA strings of equal length
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("Cannot calculate Hamming distance between sequences of different length")
 	}
+
 	distance := 0
-	for i, base := range a {
-		target, _ := utf8.DecodeRuneInString(b[i:])
-		if base != target {
+	runesA, runesB := []rune(a), []rune(b)
+	for i, base := range runesA {
+		if base != runesB[i] {
 			distance++
 		}
 	}
